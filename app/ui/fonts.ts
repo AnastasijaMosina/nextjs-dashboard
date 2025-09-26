e346b99ea6ae630418f2dbfcca3e66d9c82121e7
Anastasija Mosina <anastasija.mosina@sainsburys.co..uk>
2025-09-26 13:41:18 +0100

    styles, fonts, clsx, images

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+// file to manage fonts used in the application
+
+import { Inter, Lusitana  } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
