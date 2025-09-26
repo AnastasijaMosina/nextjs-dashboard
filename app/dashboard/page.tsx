54830776f1114dcdfa7bec18dd93abb674ae6819
Anastasija Mosina <anastasija.mosina@sainsburys.co..uk>
2025-09-26 13:41:45 +0100

    routing, layouts, pages

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+// This is the main page for the /dashboard route
+// page.tsx files in Next.js are the default pages for their respective directories
+
+export default function Page() {
+  return <p>Dashboard Page</p>;
+}
