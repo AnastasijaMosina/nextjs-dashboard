7de9f442d00864c37b6cb474fcecdf1e74fc0a1a
Anastasija Mosina <anastasija.mosina@sainsburys.co..uk>
2025-10-16 10:49:33 +0100

    chapter 11: add Search, url prop setup

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,26 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, useRouter, usePathname } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams(); // hook to read the current URL search params
+  // hook useSearchParams is used in client components only
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  function handleSearch(input: string) {
+    // Create a new URLSearchParams object based on the current search params
+    // ../..?query=searchTerm&page=2
+    const params = new URLSearchParams(searchParams.toString());
+    if(input) {
+      params.set('query', input);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +29,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
